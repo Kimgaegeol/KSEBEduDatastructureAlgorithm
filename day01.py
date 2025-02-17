0024a66b86a991291c0658e2f38f39bc67653f63
Kimgaegeol <flying0305@naver.com>
2025-02-17 10:51:50 +0900

    v1.2

--- day01.py
+++ day01.py
@@ -3,11 +3,54 @@ def is_even(n)->bool:
     짝수 판정 함수
     """
 
-    if n%2 == 0:
-        return True
+# 어떤수와 1을 and operation을 하면 어떤 수가 홀수일 경우 뒤에 1만 남고, 어떤 수가 짝수일 경우 0만 나온다
+    return not n & 1
+
+def factorial(number) -> int:
+    result = 1
+    for i in range(1, number+1):
+        result = result * i
+    return result
+
+def nCr(n, r) -> int:
+    '''
+    조합 함수
+    :param n:
+    :param r:
+    :return:
+    '''
+    numerator = factorial(n)
+    denominator = factorial(n-r) * factorial(r)
+    return int(numerator / denominator)
+
+def dec_oct_recursion(n):
+    if n == 0:
+        return ""
     else:
-        return False
+        return dec_oct_recursion(n // 8) + str(n%8)
+
+def dec_oct_repetition(n):
+    if n == 0:
+        return ""
+    else:
+        numList = []
+        result = ""
+        while(True):
+            numList.append(n%8)
+            if(n // 8 == 0):
+                break
+            n  = n // 8
+        for i in range(len(numList)-1, -1, -1):
+            result = result + str(numList[i])
+
+        return result
 
 
-n = int(input())
-print(is_even(n))
+
+if __name__ == "__main__":
+    # n = int(input("Input n : "))
+    # r = int(input("Input r : "))
+    # print(f"{n}C{r} = {nCr(n, r)}")
+    print(dec_oct_repetition(400))
+
+#========================================================================================================================
